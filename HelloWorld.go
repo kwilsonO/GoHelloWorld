fc135a488928fafdbccee90873fd23d4bb7df287
Kyle Wilson <kwilson@mtv-eng-143.mtv>
2014-06-16 17:32:35 -0700

    added dynamic port setting

--- HelloWorld.go
+++ HelloWorld.go
@@ -3,7 +3,25 @@ package main
 import(
 
   "./web"
-) 
+  "os"
+  "strconv"
+)
+
+var(
+   addr = 9999
+   listenaddr string
+)
+
+func init() {
+   if p := os.Getenv("HTTP_PORT"); p != "" {
+      var err error
+      addr, err = strconv.Atoi(p)
+      if err != nil {
+         log.Fatal(err)
+      }
+    }
+  listenAddr = fmt.Sprintf(":%d", addr)
+} 
 
 func hello(val string) string{
    return "hello " + val
@@ -11,5 +29,5 @@ func hello(val string) string{
 
 func main(){
    web.Get("/(.*)", hello)
-   web.Run("127.0.0.1:8125")
+   web.Run("127.0.0.1:" + listenaddr)
 }
